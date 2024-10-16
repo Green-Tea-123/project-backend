a133faf3ef2db1a6c16cc3c3a29c0d95f7de351b
Green-Tea-123 <junboonsoo@gmail.com>
2024-10-17 00:35:46 +0800

    Add RAG features to API (pending fixing)

--- appserver.py
+++ appserver.py
@@ -1,9 +1,15 @@
+import bs4
+from langchain_chroma import Chroma
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langserve import add_routes
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 """
 A Groq API key and Langsmith API key is needed in the .env file before the below code is called.
@@ -14,17 +20,39 @@ load_dotenv()
 model = ChatGroq(model="llama3-8b-8192")
 
 # Initialise prompt template
-systemTemplate = "Please respond to this user's query"
-promptTemplate = ChatPromptTemplate.from_messages([
-    ("system", systemTemplate), 
-    ("user", "{text}")
-])
+promptTemplate = ChatPromptTemplate.from_template("You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.\
+Question: {question} \
+Context: {context} \
+Answer:")
 
 # Initialise parser
 parser = StrOutputParser()
 
+# Initialise DB   TODO: get this part to work cos it sure as hell isn't right now, replace chroma or embedding maybe
+# bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths = ("https://www.medicalnewstoday.com/articles/165749",),
+)
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+embeddings = OllamaEmbeddings(
+    model="llama3",
+)
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+retriever = vectorstore.as_retriever()
+
+# function for formatting docs
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 # Create chain
-chain = promptTemplate | model | parser
+chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | promptTemplate
+    | model
+    | parser
+)
 
 # Initialise app
 app = FastAPI(
